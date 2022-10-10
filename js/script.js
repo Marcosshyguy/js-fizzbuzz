46ebcea49b6b3195a7ea1db99f445d876c239fff
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2022-10-10 18:29:10 +0200

    milestone 2 using switch

--- js/script.js
+++ js/script.js
@@ -21,15 +21,40 @@ for(i = 1; i <= 100; i++){
     }
     // creare un elemeto all'interno del div .container che visualizzerà BuzzFizz
     // first way
-    let buzzFizzCard = `<div class="card">${buzzFizz}</div>`;
-    console.log(buzzFizzCard)
-    document.querySelector(".row").innerHTML += buzzFizzCard;
+    // let buzzFizzCard = `<div class="card">${buzzFizz}</div>`;
+    // document.querySelector(".row").innerHTML += buzzFizzCard;
     // second way
-    // let buzzFizzCard = document.createElement("div");
-    // buzzFizzCard.classList.add("card");
-    // buzzFizzCard.innerHTML = buzzFizz;
-    // document.querySelector(".row").append(buzzFizzCard)
-    // console.log(buzzFizzCard)
+    let buzzFizzCard = document.createElement("div");
+    buzzFizzCard.classList.add("card");
+    buzzFizzCard.innerHTML = buzzFizz;
+    document.querySelector(".row").append(buzzFizzCard)
+    console.log(buzzFizzCard)
 
-
+    // milestone2  
+    // if( buzzFizzCard.innerHTML === "FizzBuzz" ){
+    //     buzzFizzCard.classList.add("buzzfizz-bg");
+    // }  
+    // else if(buzzFizzCard.innerHTML === "Buzz" ){
+    //     buzzFizzCard.classList.add("buzz-bg");
+    // }
+    // else if (buzzFizzCard.innerHTML === "Fizz" ){
+    //     buzzFizzCard.classList.add("fizz-bg");
+    // }
+    // else{
+    //     buzzFizzCard.classList.add("no-buzzfizz-bg");
+    // }
+    // switch method
+    switch(buzzFizzCard.innerHTML){
+        case "FizzBuzz":
+            buzzFizzCard.classList.add("buzzfizz-bg");
+            break;
+        case "Buzz":
+            buzzFizzCard.classList.add("buzz-bg");
+            break;
+        case "Fizz":
+            buzzFizzCard.classList.add("fizz-bg");
+            break;
+        default:
+            buzzFizzCard.classList.add("no-buzzfizz-bg");
+    }
 }
